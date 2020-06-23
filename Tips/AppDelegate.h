a7009a415aec535155008358f816e91f8581b04a
Fernando Arturo Perez <fernandoa@fb.com>
2020-06-23 13:47:51 -0700

    Initial Commit

---
+++ Tips/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tips
+//
+//  Created by Fernando Arturo Perez on 6/23/20.
+//  Copyright © 2020 Fernando Arturo Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tips
+//
+//  Created by Fernando Arturo Perez on 6/23/20.
+//  Copyright © 2020 Fernando Arturo Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
